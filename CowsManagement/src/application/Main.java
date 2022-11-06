9b14c7d21d0264e3aff6d86d52a180bd460c2970
hamzaaouni <hamzaaouni0209@gmail.com>
2022-11-07 00:44:01 +0100

    add home

---
+++ CowsManagement/src/application/Main.java
@@ -0,0 +1,37 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/interfaces/Home.fxml"));
+		    
+		    Parent root = (Parent) loader.load();
+		    
+		    Scene scene = new Scene(root);
+
+	        //primaryStage.setResizable(false);
+	        primaryStage.initStyle(StageStyle.UTILITY);
+		    
+	        primaryStage.setScene(scene);
+			
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
